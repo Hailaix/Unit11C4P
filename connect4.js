797bfc45c2e220d14ce1625e0683b4a61cc382e1
NHower <howernickoli@gmail.com>
2022-07-10 16:21:07 -0400

    Game functional

--- connect4.js
+++ connect4.js
@@ -5,83 +5,94 @@
  * board fills (tie)
  */
 
-var WIDTH = 7;
-var HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
-var currPlayer = 1; // active player: 1 or 2
-var board = []; // array of rows, each row is array of cells  (board[y][x])
+let currPlayer = 1; // active player: 1 or 2
+const board = []; // array of rows, each row is array of cells  (board[y][x])
 
 /** makeBoard: create in-JS board structure:
  *    board = array of rows, each row is array of cells  (board[y][x])
  */
 
 function makeBoard() {
-  // TODO: set "board" to empty HEIGHT x WIDTH matrix array
+  for(let y = 0; y < HEIGHT; y++){ //create HEIGHT number of rows(arrays)
+    board[y] = [];
+    for(let x = 0; x < WIDTH; x++){ //fill each row with WIDTH cells
+      board[y][x] = null; //initializes them as null
+    }
+  }
 }
 
 /** makeHtmlBoard: make HTML table and row of column tops. */
 
 function makeHtmlBoard() {
-  // TODO: get "htmlBoard" variable from the item in HTML w/ID of "board"
+  const htmlBoard = document.getElementById("board"); //get the table with id "board" to update
+  const top = document.createElement("tr"); //create the top row where pieces are placed from
+  top.setAttribute("id", "column-top"); //sets the id of the top row
+  top.addEventListener("click", handleClick); //adds the event listener
 
-  // TODO: add comment for this code
-  var top = document.createElement("tr");
-  top.setAttribute("id", "column-top");
-  top.addEventListener("click", handleClick);
-
-  for (var x = 0; x < WIDTH; x++) {
-    var headCell = document.createElement("td");
-    headCell.setAttribute("id", x);
-    top.append(headCell);
+  for (let x = 0; x < WIDTH; x++) { //creates the headcells for each column of the board
+    const headCell = document.createElement("td");
+    headCell.setAttribute("id", x); //sets each cell to have the same id as its column placement
+    top.append(headCell); //places the cell in the top row
   }
-  htmlBoard.append(top);
+  htmlBoard.append(top); //appends the top row to the html table
 
-  // TODO: add comment for this code
-  for (var y = 0; y < HEIGHT; y++) {
-    const row = document.createElement("tr");
-    for (var x = 0; x < WIDTH; x++) {
+  for (let y = 0; y < HEIGHT; y++) { //for each row of the board
+    const row = document.createElement("tr"); //create a table row
+    for (let x = 0; x < WIDTH; x++) { //then fill each row with WIDTH cells
       const cell = document.createElement("td");
-      cell.setAttribute("id", `${y}-${x}`);
+      cell.setAttribute("id", `${y}-${x}`); //gives each cell the id of its place in the board matrix
       row.append(cell);
     }
-    htmlBoard.append(row);
+    htmlBoard.append(row); //append the row to the html table
   }
 }
 
 /** findSpotForCol: given column x, return top empty y (null if filled) */
 
 function findSpotForCol(x) {
-  // TODO: write the real version of this, rather than always returning 0
-  return 0;
+  for(let y = HEIGHT-1; y >= 0; y--){ //starting at the bottom row
+    if (board[y][x] === null){ //if there is no piece in the cell, return that cell row, otherwise continue checking the cells above
+      return y;
+    }
+  }
+  return null; //if no open cell is found, return null
 }
 
 /** placeInTable: update DOM to place piece into HTML table of board */
 
 function placeInTable(y, x) {
   // TODO: make a div and insert into correct table cell
+  const piece = document.createElement("div"); //creates the div to be placed
+  const tdCell = document.getElementById(`${y}-${x}`) //gets the td to place the div in, which should have the id of its row and column
+  piece.classList.add("piece", `player${currPlayer}`); //gives the div the classes piece and playernumber(1 or 2) for css
+  tdCell.append(piece);
 }
 
 /** endGame: announce game end */
 
 function endGame(msg) {
   // TODO: pop up alert message
+  alert(msg);
 }
 
 /** handleClick: handle click of column top to play piece */
 
 function handleClick(evt) {
   // get x from ID of clicked cell
-  var x = +evt.target.id;
+  const x = +evt.target.id;
 
   // get next spot in column (if none, ignore click)
-  var y = findSpotForCol(x);
+  const y = findSpotForCol(x);
   if (y === null) {
     return;
   }
 
   // place piece in board and add to HTML table
-  // TODO: add line to update in-memory board
-  placeInTable(y, x);
+  placeInTable(y, x); //places the piece in the HTML board
+  board[y][x] = currPlayer; //sets the actual board cell to the current player number
 
   // check for win
   if (checkForWin()) {
@@ -89,10 +100,11 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-
+  if(board.every((val)=> val.every((cell) => cell !== null))){ //if every cell in every array is not null, the game is a tie
+    return endGame('Tie Game...'); 
+  }
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer === 1 ? currPlayer++ : currPlayer--;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
@@ -115,12 +127,12 @@ function checkForWin() {
 
   // TODO: read and understand this code. Add comments to help you.
 
-  for (var y = 0; y < HEIGHT; y++) {
-    for (var x = 0; x < WIDTH; x++) {
-      var horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
-      var vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
-      var diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
-      var diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
+  for (let y = 0; y < HEIGHT; y++) {
+    for (let x = 0; x < WIDTH; x++) {
+      const horiz = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+      const vert = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+      const diagDR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+      const diagDL = [[y, x], [y + 1, x - 1], [y + 2, x - 2], [y + 3, x - 3]];
 
       if (_win(horiz) || _win(vert) || _win(diagDR) || _win(diagDL)) {
         return true;
